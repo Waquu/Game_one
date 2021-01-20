e88b5c6cd66a639a149668f7cae2ce9a41d7e2fb
Waquu <d.Waqu2044@gmail.com>
2021-01-20 19:51:55 +0300

    First commit, Added moving Hero, add ed worm, fly, bomber, slime, barnacle, added Tilemap

---
+++ Assets/Scripts/Destroyable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyable : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().
+            AddForce(transform.up *
+            collision.gameObject.GetComponent<Player>().jumpHeight,
+            ForceMode2D.Impulse);
+            gameObject.GetComponentInParent<Enemy>().StartDeath();
+        }
+    }
+}
